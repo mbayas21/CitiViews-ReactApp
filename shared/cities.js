16a60105c19119bb44ec114626b13679b5362075
Mauricio Bayas <mbayas1993@gmail.com>
2021-03-16 00:08:50 -0400

    Added first shared files and project images

---
+++ shared/cities.js
@@ -0,0 +1,26 @@
+export const CITIES = [
+    {
+
+        id: 0,
+        name: 'Charlotte',
+        state: 'North Carolina',
+        image: '/assests/imgs/imgs/Charlotte-1.jpg'
+
+    },
+    {
+
+        id: 1,
+        name: 'Charleston',
+        state: 'South Carolina',
+        image: '/assests/imgs/imgs/seattle-md.jpg'
+
+    },
+    {
+
+        id: 3,
+        name: 'Seattle',
+        state: 'Washington',
+        image: '/assests/imgs/imgs/charleston-1.jpeg'
+
+    }
+];
